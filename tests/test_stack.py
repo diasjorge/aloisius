04f1aff75a0fe364428a436bb42fa3ea9c82f15e
Jorge Dias <jorge@mrdias.com>
2017-03-29 16:43:10 +0200

    Spy instance for pypy

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -31,14 +31,14 @@ def test_stack_is_updated_when_already_exists(monkeypatch, mocker):
 
     monkeypatch.setattr(Stack, '_create', lambda x: False)
 
-    spy = mocker.spy(Stack, '_update')
-
     update_stack = Stack(
         StackName='dummy',
         TargetState='present',
         RegionName='eu-west-1',
         TemplateBody=json.dumps(dummy_template)
     )
+
+    spy = mocker.spy(update_stack, '_update')
 
     assert re.match("vpc-[a-z0-9]+", update_stack.outputs['VPC'])
 
